73633301bbb2e636e0d92791094d51ba6343e5af
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2023-07-06 14:42:03 -0700

    Develop structure to the page

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const container = document.createElement('div');
 container.setAttribute('id', 'gridContainer');
 
-
 document.querySelector('body').appendChild(container);
+
+for (let i = 0; i < 16; i++) {
+
+    const row = document.createElement('div');
+    row.setAttribute('class', 'row');
+
+    container.appendChild(row);
+    
+    for (let j = 0; j < 16; j++) {
+        const div = document.createElement('div');
+        div.setAttribute('class', 'pixel');
+
+        row.appendChild(div);
+    }
+}
