91c08f41b0a20837c82fe5cb583863201156bf30
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2023-07-19 00:35:04 -0700

    Add a responsive user input box and make grid flexible

--- script.js
+++ script.js
@@ -1,34 +1,63 @@
+const formToResize = document.querySelector('#formToResize');
+let column = 16;
 
 
-const answer = prompt("Please enter the length of the grid you'd like for the grid box.");
-
-
-const column = answer; 
-
-const gridBox = document.createElement('div');
-gridBox.setAttribute('id', 'gridBox');
-gridBox.setAttribute('style', `grid-template-columns: repeat(${column}, 1fr);`);
-
-
-document.querySelector('body').appendChild(gridBox);
-
-
-for (let i = 0; i < column ** 2; i++) {
-
-    const div = document.createElement('div');
-    div.setAttribute('class', 'cell');
-
-    gridBox.appendChild(div);
-
+function clearGame() {
+    const cells = document.querySelectorAll('.cells');
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = 'white';
+    });
 }
 
+function createGridContainer(dimension) {
 
-const cells = document.querySelectorAll('.cell');
-cells.forEach((cell) => {
-    cell.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = 'black';
-    });
-});
+    console.log(dimension);
+    const gridBox = document.createElement('div');
+    gridBox.setAttribute('id', 'gridBox');
+    document.querySelector('body').appendChild(gridBox);
+}
+
+function createGrid(dimension) {
+
+    const gridBox = document.querySelector('#gridBox');
+    gridBox.setAttribute('style', `grid-template-columns: repeat(${dimension}, 1fr);`);
+    console.log(dimension);
+
+    for (let i = 0; i < dimension ** 2; i++) {
+
+        const div = document.createElement('div');
+        div.setAttribute('class', 'cell');
+        gridBox.appendChild(div);
+
+        div.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = 'black';
+        });
+    
+    }
+}
+
+createGridContainer(column);
+createGrid(column);
+
+function removeGrid() {
+    const gridBox = document.querySelector('#gridBox');
+    while(gridBox.firstChild) {
+        gridBox.removeChild(gridBox.firstChild);
+    }
+}
+
+function submitSize(event) {
+
+    event.preventDefault();
+    console.log("hello");
+    column = formToResize.querySelector('#size').value;
+    alert(column);
+    clearGame();
+    removeGrid();
+    createGrid(column);
+}
+
+formToResize.addEventListener('submit', submitSize);
 
 
 const clearButton = document.createElement('button');
@@ -37,8 +66,3 @@ document.querySelector('body').appendChild(clearButton);
 
 clearButton.addEventListener('click', clearGame);
 
-function clearGame() {
-    cells.forEach((cell) => {
-        cell.style.backgroundColor = 'white';
-    });
-}
